15af34657b8eb84835e5c3510e6c879eb270f181
IJE2704 <mh1669101@gmail.com>
2023-08-25 19:33:15 +0600

    route add with home contact about page

--- src/main.jsx
+++ src/main.jsx
@@ -6,16 +6,35 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import './index.css'
+import Home from './Components/Home/Home.jsx';
+import About from './Components/About/About.jsx';
+import Contact from './Components/Contact/Contact.jsx';
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <div>Hello world!</div>,
+//   },
+//   {
+//     path: "/app",
+//     element: <App></App>,
+//   }
+// ]); 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <div>Hello world!</div>,
-  },
-  {
-    path: "/app",
-    element: <App></App>,
+    path: '/',
+    element: <Home></Home>,
+    children:[
+      {
+        path: '/about',
+        element: <About></About>,
+      },
+      {
+        path: '/contact',
+        element: <Contact></Contact>
+      }
+    ]
   }
-]); 
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
